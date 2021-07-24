95ac4929663b9dda16dd222658cf7ca753b20a82
canserkanuren <canserkan.uren@gmail.com>
2021-07-24 21:02:03 +0200

    :sparkles:

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,15 +1,44 @@
 import { Config } from '@stencil/core';
 import {angularOutputTarget, ValueAccessorConfig} from '@stencil/angular-output-target';
 
+const TARGET_ATTRS = {
+  CHECKED: 'checked',
+  VALUE: 'value'
+};
+
+const valueAcessorConfig: ValueAccessorConfig[] = [
+  {
+    elementSelectors: 'ui-input[type=text]',
+    type: 'text',
+    event: 'myInput',
+    targetAttr: TARGET_ATTRS.VALUE
+  },
+  {
+    elementSelectors: 'ui-input[type=number]',
+    type: 'number',
+    event: 'myInput',
+    targetAttr: TARGET_ATTRS.VALUE
+  }
+];
+
 export const config: Config = {
   namespace: 'ui-component',
   outputTargets: [
     angularOutputTarget({
       componentCorePackage: '@raydeeeen/ui-component',
-      directivesProxyFile: ''
+      directivesProxyFile: './@raydeeeen/ui-component-angular/components.ts',
+      valueAccessorConfigs: valueAcessorConfig
     }),
     {
       type: 'dist',
+      esmLoaderPath: '../loader',
     },
-  ],
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    }
+  ]
 };
