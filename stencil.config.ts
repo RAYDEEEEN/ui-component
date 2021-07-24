6132d0b276f180e45614571e8bd1e7d51c1dab4e
canserkanuren <canserkan.uren@gmail.com>
2021-07-24 15:57:28 +0200

    :hammer:

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,21 +1,15 @@
 import { Config } from '@stencil/core';
+import {angularOutputTarget, ValueAccessorConfig} from '@stencil/angular-output-target';
 
 export const config: Config = {
   namespace: 'ui-component',
   outputTargets: [
+    angularOutputTarget({
+      componentCorePackage: '@raydeeeen/ui-component',
+      directivesProxyFile: ''
+    }),
     {
       type: 'dist',
-      esmLoaderPath: '../loader',
-    },
-    {
-      type: 'dist-custom-elements-bundle',
-    },
-    {
-      type: 'docs-readme',
-    },
-    {
-      type: 'www',
-      serviceWorker: null, // disable service workers
     },
   ],
 };
